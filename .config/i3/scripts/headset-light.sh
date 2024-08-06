f0a79d3fcefcafa066e0e7b82d2d33afcfab89aa
tom-jm03 <Lilleboysweetie@gmail.com>
2024-08-06 23:34:43 +0200

    [FEATURE] add .zshrc

--- .config/i3/scripts/headset-light.sh
+++ .config/i3/scripts/headset-light.sh
@@ -5,5 +5,5 @@ CONNECT=$(headsetcontrol | grep "G935")
 
 if [ "$CONNECT" != "" ]; then
     headsetcontrol -l 0
-    dunstify "G935 turned off the lights"
+    dunstify "G935" "turned off the lights" --raw_icon=/usr/share/icons/Adwaita/16x16/devices/audio-headset.png
 fi

6f9a105d93d7e4981085005ef51c7f33dc88bd83
tom-jm69 <tom-jm69@users.noreply.github.com>
2024-12-21 21:39:47 +0100

    [TASK] add pacman backup script

--- .config/i3blocks/scripts/date/date.sh
+++ .config/i3blocks/scripts/date/date.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Define the default and alternate date formats
-default_format="LC_ALL=\"de_DE.UTF-8\" date '+%Y %B %d %A ' | awk '{ printf \"%s <span color=\\\"#ed8796\\\">%s %s</span> %s <span color=\\\"#ed8796\\\">%s%s</span>\n\", \$1, \$2, \$3, \$4, \$5, \$6 }'"
+default_format="LC_ALL=\"de_DE.UTF-8\" date '+%A %d %B %Y' | awk '{ printf \"<span color=\\\"#ed8796\\\">%s</span> %s <span color=\\\"#ed8796\\\">%s %s</span>\n\", substr(\$1, 1, 2), \$2, \$3, \$4 }'"
 alternate_format="date '+%V. KW %d.%m.%Y'"
 
 # Check if the left button was clicked
 if [ "$BLOCK_BUTTON" == "1" ]; then
     # Display the alternate format
     eval "$alternate_format"
-
-    # Wait for 5 seconds
 else
     # Display the default format
     eval "$default_format"

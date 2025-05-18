bd7a27d3cd5c3718acdf969065456b3bc258573f
tom-jm69 <tom-jm69@users.noreply.github.com>
2025-05-18 18:33:04 +0200

    [TASK] update check-installed.sh

--- .config/hypr/scripts/check-installed.sh
+++ .config/hypr/scripts/check-installed.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-INSTALLED=("solaar" "kaki" "waybar" "wl-paste" "cliphist" "gsettings" "dex" "dunst" "alacritty" "flameshot" "wofi" "dmenu" "rofi" "zsh" "adw-gtk-theme" "adw-gtk3" "adwaita-fonts" "alsa-plugins" "ansible" "ansible-core" "binwalk" "bitwarden" "spotify" "docker" "ffmpeg" "firefox" "git")
+INSTALLED=("solaar" "waybar" "wl-clipboard" "cliphist" "gsettings" "dex" "dunst" "alacritty" "flameshot" "wofi" "dmenu" "rofi" "zsh" "adw-gtk-theme" "adw-gtk3" "adwaita-fonts" "alsa-plugins" "ansible" "ansible-core" "binwalk" "bitwarden" "spotify" "docker" "ffmpeg" "firefox" "git")
 
 missing=()
 
 for program in "${INSTALLED[@]}"
 do
     if ! command -v "$program" &> /dev/null; then
-        missing+=("$program")
-        notify-send -w "Missing program" "Program '$program' is not installed or not in PATH."
+        if ! pacman -Qi "$program" &> /dev/null; then
+            missing+=("$program")
+            notify-send -w "Missing program" "Program '$program' is not installed or not in PATH."
+        fi
     fi
 done
 

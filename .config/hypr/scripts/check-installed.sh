5777d47f2911a295ec88e5b8c203aa0d691b0b37
tom.mueller <tom.mueller@bitpoint.cloud>
2025-05-26 10:19:47 +0200

    [TASK] add more stuff

---
+++ .config/hypr/scripts/check-installed.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+INSTALLED=("solaar" "waybar" "wl-clipboard" "cliphist" "gsettings" "dex" "dunst" "alacritty" "flameshot" "wofi" "dmenu" "rofi" "zsh" "adw-gtk-theme" "adw-gtk3" "adwaita-fonts" "alsa-plugins" "ansible" "ansible-core" "binwalk" "bitwarden" "spotify" "docker" "ffmpeg" "firefox" "git")
+
+missing=()
+
+for program in "${INSTALLED[@]}"
+do
+    if ! command -v "$program" &> /dev/null; then
+        if ! pacman -Qi "$program" &> /dev/null; then
+            missing+=("$program")
+            notify-send -w "Missing program" "Program '$program' is not installed or not in PATH."
+        fi
+    fi
+done
+
+if [ ${#missing[@]} -eq 0 ]; then
+    exit 0
+fi

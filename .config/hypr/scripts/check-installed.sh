754460334045959586ca34c5da90e5195c49648c
tom-jm69 <tom-jm69@users.noreply.github.com>
2025-06-24 12:54:54 +0200

    [TASK] update scripts

--- .config/hypr/scripts/check-installed.sh
+++ .config/hypr/scripts/check-installed.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-INSTALLED=("solaar" "zoxide" "waybar" "whois" "wl-clipboard" "cliphist" "gsettings" "dex" "dunst" "alacritty" "flameshot" "wofi" "dmenu" "rofi" "zsh" "adw-gtk-theme" "adw-gtk3" "adwaita-fonts" "alsa-plugins" "ansible" "ansible-core" "binwalk" "bitwarden" "spotify-launcher" "docker" "ffmpeg" "firefox" "git" "spicetify" "go" "cargo" "rustc")
+INSTALLED=("gnome-themes-extra" "pre-commit" "adwaita-qt5-git" "adwaita-qt6-git" "solaar" "zoxide" "waybar" "whois" "wl-clipboard" "cliphist" "gsettings" "dex" "dunst" "alacritty" "flameshot" "wofi" "dmenu" "rofi" "zsh" "adw-gtk-theme" "adwaita-fonts" "alsa-plugins" "ansible" "ansible-core" "binwalk" "bitwarden" "spotify-launcher" "docker" "ffmpeg" "firefox" "git" "spicetify" "go" "cargo" "rustc")
 
 missing=()
 
 for program in "${INSTALLED[@]}"
 do
     if ! command -v "$program" &> /dev/null; then
-        if ! pacman -Qi "$program" &> /dev/null; then
+        if ! yay -Qi "$program" &> /dev/null; then
             missing+=("$program")
             notify-send -w "Missing program" "Program '$program' is not installed or not in PATH."
         fi

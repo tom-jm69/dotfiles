01205b825147e2d3b0806ca5f1c4cefcd258a396
tom-jm69 <tom-jm69@users.noreply.github.com>
2025-08-23 22:25:08 +0200

    [TASK] add games to dont kill script and change app menu to sherlock

--- .config/hypr/scripts/dont-kill-steam.sh
+++ .config/hypr/scripts/dont-kill-steam.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Get active window's class
-window_class=$(hyprctl activewindow -j | jq -r '.class')
+# Get active window's class and lowercase it
+window_class=$(hyprctl activewindow -j | jq -r '.class' | tr '[:upper:]' '[:lower:]')
 
-# Check if it's a Steam game
-if [[ "$window_class" == steam_app_* ]]; then
-    # Do nothing if it's a Steam game
-    exit 0
-else
-    # Kill the active window
-    hyprctl dispatch killactive ""
-fi
+dont_kill=("minecraft" "cs2" "steam")
+
+# Check if the window class is in the dont_kill array
+for app in "${dont_kill[@]}"; do
+    if [[ "$window_class" == "$app"* ]]; then
+        # Do nothing if it's in the dont_kill list
+        exit 0
+    fi
+done
+
+# Kill the active window if it's not in the dont_kill list
+hyprctl dispatch killactive ""

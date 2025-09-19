e113fbad5ed9f642ca22ed0c992e6f0faf8926cd
tom-jm69 <tom-jm69@users.noreply.github.com>
2025-09-19 21:44:56 +0200

    task: change toggle-mic-mute to send nicer notification

--- .config/hypr/scripts/toggle-mic-mute.sh
+++ .config/hypr/scripts/toggle-mic-mute.sh
@@ -3,13 +3,23 @@
 SOURCE=$(pactl get-default-source)
 MONITOR_SCRIPT=~/dotfiles/.config/hypr/scripts/mic-monitor.sh
 PIDFILE="/tmp/mic_monitor.pid"
+ICON_MIC_ON="microphone-sensitivity-high-symbolic"
+ICON_MIC_OFF="microphone-sensitivity-muted-symbolic"
+APP_NAME="Mic Toggle"
 
 MUTE_STATE=$(pactl get-source-mute "$SOURCE" | awk '{print $2}')
 
 if [[ "$MUTE_STATE" == "yes" ]]; then
     pactl set-source-mute "$SOURCE" 0
-    notify-send -t 30000 "🎤 Mic unmuted"
-
+    notify-send \
+        -a "$APP_NAME" \
+        -i "$ICON_MIC_ON" \
+        -u normal \
+        -t 2500 \
+        -e \
+        "🎤 Mic is ON" \
+        "You are live, talk away!"
+    
     # Kill background monitor if running
     if [[ -f "$PIDFILE" ]]; then
         kill "$(cat "$PIDFILE")" 2>/dev/null
@@ -17,8 +27,15 @@ if [[ "$MUTE_STATE" == "yes" ]]; then
     fi
 else
     pactl set-source-mute "$SOURCE" 1
-    notify-send -t 300000 "🔇 Mic muted"
-
+    notify-send \
+        -a "$APP_NAME" \
+        -i "$ICON_MIC_OFF" \
+        -u normal \
+        -t 2500 \
+        -e \
+        "🔇 Mic is OFF" \
+        "You are muted now. Shhh..."
+    
     # Start background monitoring
     bash "$MONITOR_SCRIPT" "$SOURCE" &
     echo $! > "$PIDFILE"
